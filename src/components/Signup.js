fb0b455a0613eed520da2f3381e115c9f42b6800
mohasal0101 <mohasal0101@gmail.com>
2022-09-23 00:39:24 +0300

    modified frontend to bearerAuth + cookies

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import "../App.css";
+import cookies from 'react-cookies';
 
 
 
@@ -17,36 +18,36 @@ function Signup() {
         const user = {
             'username': e.target.username.value,
             'password': e.target.password.value,
-            'email': e.target.email.value,
         };
         await axios.post(
             `https://whiteboarding-backend-401.herokuapp.com/signup`,
             user
         ).then( (res) => {
             if (res.status === 200) {
-                localStorage.setItem('token', true);
+                localStorage.setItem('token', res.data.token);
                 window.location.href = '/posts';
+                cookies.save('auth', res.data.token);
+                cookies.save('user', res.data.user);
+                cookies.save('username', res.data.user.username);
+                cookies.save('email', res.data.user.email);
+                cookies.save('token', res.data.token);                
             } 
           }).catch( (err) => {
-            alert('Username or email already exists'); 
+            alert('Username already exists'); 
         } );
+
     };
     };
     return (
         <Container>
         <div className="container">
-        <div class="ocean">
-            <div class="wave"></div>
-            </div>
         <Form onSubmit={handleSubmit}>
                 <div className="form-group">
+                <h1 className="form-header">Sign in form</h1>
                     <label htmlFor="username">Username</label>
                     <input type="text" className="form-control" id="username" name="username" />
                 </div>
-                <div className="form-group">
-                    <label htmlFor="email">Email</label>
-                    <input type="email" className="form-control" id="email" name="email" />
-                </div>
+                
                 <div className="form-group">
                     <label htmlFor="password">Password</label>
                     <input type="password" className="form-control" id="password" name="password" />
